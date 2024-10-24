ae32e8d437672d1ff0600cff1d759b4e33233100
pedroaugustosgs <pedrovilelasgs@unifei.edu.br>
2024-10-24 16:42:28 -0300

    Avaliação da Republica
    Nao testei completamente e ainda to pesquisando como faz pra botar estrelas aviadadas

--- rate_io/lib/routes.dart
+++ rate_io/lib/routes.dart
@@ -3,12 +3,14 @@ import 'loginScream.dart'; // Importar suas páginas
 import 'registerScream.dart';
 import 'repRegisterScream.dart';
 import 'homeScream.dart';
+import 'avaliaRepScream.dart'; // Import the AvaliaRepScreen
 
 class Routes {
   static const String login = '/';
   static const String registerScream = '/registerScream';
   static const String homeScream = '/homeScream';
   static const String repRegisterScream = '/repRegisterScream';
+  static const String avaliaRepScream = '/avaliaRepScream'; // Add a constant for AvaliaRepScreen
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -20,6 +22,8 @@ class Routes {
         return MaterialPageRoute(builder: (_) => HomePage());
       case repRegisterScream:
         return MaterialPageRoute(builder: (_) => RepRegisterPage());
+      case avaliaRepScream:
+        return MaterialPageRoute(builder: (_) => AvaliaRepScreen()); // Add the route for AvaliaRepScreen
       default:
         return MaterialPageRoute(builder: (_) => LoginPage()); // Tela padrão
     }
