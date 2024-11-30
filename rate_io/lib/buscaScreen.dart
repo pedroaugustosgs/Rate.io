2ebf15830a95eb614adc07245b6582ca4507b572
MateusAMSouza <mateusalexandrems@gmail.com>
2024-11-30 19:40:29 -0300

    Atualização da tela de busca para redirecionar ao perfil dos moradores

--- rate_io/lib/buscaScreen.dart
+++ rate_io/lib/buscaScreen.dart
@@ -1,22 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 //import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rate_io/perfilUsuarioScreen.dart';
 
 class BuscaScreen extends StatefulWidget {
   @override
   _BuscaScreenState createState() => _BuscaScreenState();
 }
 
+Widget _navigatePerfilUsuario({required Map<String, dynamic> morador}) {
+  return PerfilUsuarioScreen(morador: morador);
+}
+
 class _BuscaScreenState extends State<BuscaScreen> {
 
-  List _todosResultados = [];
-  List _resultadoBusca = [];
+  List<Map<String, dynamic>> _todosResultados = [];
+  List<Map<String, dynamic>> _resultadoBusca = [];
   final TextEditingController _buscaController = TextEditingController();
 
 
   @override
   void initState() {
     _buscaController.addListener(_onSearchChanged);
+    _fetchMoradores();
     super.initState();
   }
 
@@ -25,49 +31,47 @@ class _BuscaScreenState extends State<BuscaScreen> {
     buscaResultados();
   }
 
-  buscaResultados() {
-    var mostraResultados = [];
-    if(_buscaController.text != "")
-    {
-      for(var clientSnapshot in _todosResultados)
-      {
-        var nome = clientSnapshot['nome'].toString().toLowerCase();
-        if(nome.contains(_buscaController.text.toLowerCase()))
-        {
-          mostraResultados.add(clientSnapshot);
+  Future<void> _fetchMoradores() async {
+    try {
+      final querySnapshot = await FirebaseFirestore.instance.collection('moradores').orderBy('nome').get();
+      final results = querySnapshot.docs.map((doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        data['id'] = doc.id; // Add the document ID to the data
+        return data;
+      }).toList();
+
+      setState(() {
+        _todosResultados = results;
+      });
+      buscaResultados();
+    } catch (e) {
+      print('Erro ao buscar moradores: $e');
+    }
+  }
+
+  void buscaResultados() {
+    List<Map<String, dynamic>> resultadosFiltrados = [];
+    if (_buscaController.text.isNotEmpty) {
+      for (var morador in _todosResultados) {
+        final nome = morador['nome']?.toString().toLowerCase() ?? '';
+        if (nome.contains(_buscaController.text.toLowerCase())) {
+          resultadosFiltrados.add(morador);
         }
       }
     } else {
-      mostraResultados = List.from(_todosResultados);
+      resultadosFiltrados = List.from(_todosResultados);
     }
 
     setState(() {
-      _resultadoBusca = mostraResultados;
+      _resultadoBusca = resultadosFiltrados;
     });
   }
-
-  getClientStream() async {
-    var data = await FirebaseFirestore.instance.collection('moradores').orderBy('nome').get();
-
-    setState(() {
-      _todosResultados = data.docs;
-    });
-    buscaResultados();
-  }
-
-  
 
   @override
   void dispose() {
     _buscaController.removeListener(_onSearchChanged);
     _buscaController.dispose();
     super.dispose();
-  }
-
-  @override
-  void didChangeDependencies() {
-    getClientStream();
-    super.didChangeDependencies();
   }
   
 
@@ -82,16 +86,30 @@ class _BuscaScreenState extends State<BuscaScreen> {
           ),
         )
       ),
-      body: ListView.builder(
-        itemCount: _resultadoBusca.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_resultadoBusca[index]['nome'],),
-            subtitle: Text(_resultadoBusca[index]['faculdade'],),
-            trailing: Text(_resultadoBusca[index]['email'],),
-          );
-        }
-      ),
+      body: _resultadoBusca.isEmpty
+          ? Center(child: Text('Nenhum resultado encontrado'))
+          : ListView.builder(
+            itemCount: _resultadoBusca.length,
+            itemBuilder: (context, index) {
+              final perfil = _resultadoBusca[index];
+              return ListTile(
+                title: Text(_resultadoBusca[index]['nome'],),
+                subtitle: Text(_resultadoBusca[index]['faculdade'],),
+                trailing: IconButton(
+                  icon: Icon(Icons.label_important_outline_sharp),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                          _navigatePerfilUsuario(morador: perfil),
+                      ),
+                    );
+                  },
+                )
+              );
+            }
+          ),
     );
   }
 }
---
+++ rate_io/lib/perfilUsuarioScreen.dart
@@ -0,0 +1,114 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:rate_io/classes/morador.dart';
+import 'package:rate_io/classes/sexo.dart';
+//import 'classes/morador.dart';
+import 'routes.dart';
+
+class PerfilUsuarioScreen extends StatefulWidget {
+  final Map<String, dynamic> morador;
+  PerfilUsuarioScreen({required this.morador});
+
+  @override
+  _PerfilUsuarioState createState() => _PerfilUsuarioState();
+}
+
+class _PerfilUsuarioState extends State<PerfilUsuarioScreen> {
+  late String _id;
+
+  @override
+  void initState() {
+    super.initState();
+    _id = widget.morador['id'];
+  }
+
+  Future<Map<String, dynamic>> _fetchUsuarioERep() async {
+    try {
+      final usuarioDoc = await FirebaseFirestore.instance.collection('moradores').doc(_id).get();
+      if (!usuarioDoc.exists || usuarioDoc.data() == null) {
+        throw Exception('Morador não encontrado.');
+      }
+      final moradorData = usuarioDoc.data()!;
+
+      String? repId = moradorData['repId'];
+      String? nomeRep;
+      print(repId);
+      if (repId != null && repId.isNotEmpty) {
+        final republicDoc = await FirebaseFirestore.instance.collection('republicas').doc(repId).get();
+        nomeRep = republicDoc.data()?['nome'] ?? 'República não encontrada';
+      }
+      print(nomeRep);
+      return {
+        'morador': moradorData,
+        'nomeRep': nomeRep,
+      };
+    } catch (e) {
+      throw Exception('Erro ao carregar dados: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil do Usuário'),
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _fetchUsuarioERep(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erro ao carregar dados'));
+          } else if (!snapshot.hasData) {
+            return Center(child: Text('Dados não encontrados'));
+          }
+
+          final usuario = snapshot.data!['morador'] as Map<String, dynamic>;
+          final nomeRep = snapshot.data!['nomeRep'] as String?;
+          print(nomeRep);
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Nome: ${usuario['nome']}', style: TextStyle(fontSize: 24)),
+                Text('Sexo: ${SexoExtension.fromString(usuario['sexo']).name}', style: TextStyle(fontSize: 24)),
+                Text('Rep: ${nomeRep ?? 'Sem república'}', style: TextStyle(fontSize: 24)),
+                Text('Telefone: ${usuario['telefone']}', style: TextStyle(fontSize: 24)),
+                Text('Curso: ${usuario['curso']}', style: TextStyle(fontSize: 24)),
+                Text('Faculdade: ${usuario['faculdade']}', style: TextStyle(fontSize: 24)),
+                Text('Data de nascimento: ${DateFormat('dd/MM/yyyy').format((usuario['dataNascimento'] as Timestamp).toDate())}', style: TextStyle(fontSize: 24)),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Voltar'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navegar para a tela de avaliações
+                      },
+                      child: Text('Avaliações'),
+                    ),
+                    ElevatedButton(
+                      onPressed: (){
+                        Navigator.of(context).pushNamed(Routes.editarPerfilMoradorScreen); //////MUDAR PARA A TELA DE CONVIDAR
+                      },
+                      child: Text('Convidar'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
