54a33bd3d6aa3f3a3aaf9dd6c6027dc4f55df85c
MateusAMSouza <mateusalexandrems@gmail.com>
2024-12-05 16:03:43 -0300

    Subsituição de 'MeuPerfil' por 'PerfilUsuario' com usuário da sessão como argumento

--- rate_io/lib/perfilUsuarioScreen.dart
+++ rate_io/lib/perfilUsuarioScreen.dart
@@ -7,6 +7,7 @@ import 'package:rate_io/avaliaMoradorScreen.dart';
 import 'package:rate_io/classes/moradorProvider.dart';
 import 'package:rate_io/classes/repProvider.dart';
 import 'package:rate_io/convidarUsuarioScreen.dart';
+import 'package:rate_io/routes.dart';
 import 'package:rate_io/verAvaliacoesScreen.dart';
 import 'package:rate_io/classes/sexo.dart';
 
@@ -19,6 +20,8 @@ class PerfilUsuarioScreen extends StatefulWidget {
 }
 
 class _PerfilUsuarioState extends State<PerfilUsuarioScreen> {
+  int _currentIndex = 1;
+  int _anteriorIndex = 0;
   late String _id;
 
   @override
@@ -74,79 +77,137 @@ class _PerfilUsuarioState extends State<PerfilUsuarioScreen> {
 
           final usuario = snapshot.data!['morador'] as Map<String, dynamic>;
           final nomeRep = snapshot.data!['nomeRep'] as String?;
-          print(nomeRep);
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Nome: ${usuario['nome']}', style: TextStyle(fontSize: 24)),
-                Text('Sexo: ${SexoExtension.fromString(usuario['sexo']).name}', style: TextStyle(fontSize: 24)),
-                Text('Rep: ${nomeRep ?? 'Sem república'}', style: TextStyle(fontSize: 24)),
-                Text('Telefone: ${usuario['telefone']}', style: TextStyle(fontSize: 24)),
-                Text('Curso: ${usuario['curso']}', style: TextStyle(fontSize: 24)),
-                Text('Faculdade: ${usuario['faculdade']}', style: TextStyle(fontSize: 24)),
-                Text('Data de nascimento: ${DateFormat('dd/MM/yyyy').format((usuario['dataNascimento'] as Timestamp).toDate())}', style: TextStyle(fontSize: 24)),
-                SizedBox(height: 20),
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Voltar'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
+
+          return Scaffold(
+            bottomNavigationBar: (usuario['id'] == meuPerfil!.id)
+                ? BottomNavigationBar(
+                    currentIndex: _currentIndex,
+                    onTap: (index) {
+                      if (_currentIndex == index && _anteriorIndex == index) {
+                        return;
+                      }
+                      setState(() {
+                        _anteriorIndex = _currentIndex;
+                        _currentIndex = index;
+                      });
+                      if (index == 0) {
+                        Navigator.pushNamed(
+                          context,
+                          Routes.buscaScreen,
+                          arguments: meuPerfil,
+                        );
+                      } else if (index == 1) {
+                        Navigator.pushNamed(
+                          context,
+                          Routes.homeScreen,
+                        );
+                      } else if (index == 2) {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => VerAvaliacoesScreen(usuario: usuario),
+                            builder: (context) => PerfilUsuarioScreen(morador: meuPerfil.toMap()),
                           ),
                         );
-                      },
-                      child: Text('Avaliações'),
-                    ),
-                    if(usuario['repId'] == null && meuPerfil!.id == minhaRep!.moradorADMId)
+                      }
+                    },
+                    items: [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.search),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.account_balance),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.person),
+                        label: '',
+                      ),
+                    ],
+                  )
+                : null,
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Nome: ${usuario['nome']}', style: TextStyle(fontSize: 24)),
+                  Text('Sexo: ${SexoExtension.fromString(usuario['sexo']).name}', style: TextStyle(fontSize: 24)),
+                  Text('Rep: ${nomeRep ?? 'Sem república'}', style: TextStyle(fontSize: 24)),
+                  Text('Telefone: ${usuario['telefone']}', style: TextStyle(fontSize: 24)),
+                  Text('Curso: ${usuario['curso']}', style: TextStyle(fontSize: 24)),
+                  Text('Faculdade: ${usuario['faculdade']}', style: TextStyle(fontSize: 24)),
+                  Text('Data de nascimento: ${DateFormat('dd/MM/yyyy').format((usuario['dataNascimento'] as Timestamp).toDate())}', style: TextStyle(fontSize: 24)),
+                  SizedBox(height: 20),
+                  Column(
+                    children: [
                       ElevatedButton(
-                        onPressed: (){
-                           Navigator.push(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Voltar'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => ConvidarUsuarioScreen(usuario: usuario),
+                              builder: (context) => VerAvaliacoesScreen(usuario: usuario),
                             ),
                           );
                         },
-                        child: Text('Convidar'),
+                        child: Text('Avaliações'),
                       ),
-                    if(usuario['repId'] == meuPerfil!.repId)...[
-                      ElevatedButton(
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => AvaliaMoradorScreen(morador: usuario),
-                            )
-                          );
-                        }, 
-                        child: Text('Avaliar'),
-                      ),
-                      if(meuPerfil.id == minhaRep!.moradorADMId)
+                      if (usuario['id'] == meuPerfil.id) ...[
                         ElevatedButton(
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => AtribuirTarefaScreen(morador: usuario),
-                            )
-                          );
-                        }, 
-                        child: Text('Atribuir Tarefa'),
-                      ),
-                    ]
-                  ],
-                ),
-              ],
+                          onPressed: () {
+                            Navigator.of(context).pushNamed(Routes.editarPerfilMoradorScreen);
+                          },
+                          child: Text('Editar meu perfil'),
+                        ),
+                      ] else ...[
+                        if (usuario['repId'] == null && meuPerfil.id == minhaRep!.moradorADMId)
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ConvidarUsuarioScreen(usuario: usuario),
+                                ),
+                              );
+                            },
+                            child: Text('Convidar'),
+                          ),
+                        if (usuario['repId'] == meuPerfil.repId) ...[
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AvaliaMoradorScreen(morador: usuario),
+                                ),
+                              );
+                            },
+                            child: Text('Avaliar'),
+                          ),
+                          if (meuPerfil.id == minhaRep!.moradorADMId)
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => AtribuirTarefaScreen(morador: usuario),
+                                  ),
+                                );
+                              },
+                              child: Text('Atribuir Tarefa'),
+                            ),
+                        ],
+                      ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           );
         },
